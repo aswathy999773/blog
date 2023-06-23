9dd8d2cde68b35776c3ecace695b5dc93c97a567
aswathy999773 <aswathysuresh251#@gmail.com>
2023-06-23 10:02:27 +0530

    blog

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from "react";
+import Home from "./Home";
+import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
+import Form from "./Form";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/form" element={<Form />} />
+      </Routes>
+    </div>
+  );
 }
-
-
-
